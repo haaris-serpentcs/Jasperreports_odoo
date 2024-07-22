c679a76888c084a80e9d9d2b98dce4ed6f07e9e8
Serpent Consulting Services Pvt. Ltd. <serpentcs.lp@gmail.com>
2024-07-22 16:47:37 +0530

    Update __manifest__.py

--- jasper_reports/__manifest__.py
+++ jasper_reports/__manifest__.py
@@ -33,7 +33,7 @@
 
 {
     "name": "Jasper Reports",
-    "version": "16.0.0.1.0",
+    "version": "15.0.0.1.0",
     "summary": '''This module integrates Jasper Reports with Odoo.''',
     "author": "NaN·tic, Serpent Consulting Services Pvt. Ltd.",
     "website": "http://www.nan-tic.com, http://www.serpentcs.com",
